b44e3f162156acd3a7464194529f63832f1f0307
Anton Zickler <azic02@proton.me>
2025-04-10 14:22:49 +0200

    Use np.corrcoef instead of scipy.stats.pearsonr
    for spatial correlation calculation, because of better performance,
    presumably because no confidence intervals are calculated.

--- anaspike/functions/statistical_quantities.py
+++ anaspike/functions/statistical_quantities.py
@@ -2,7 +2,6 @@ from typing import Iterable, Sequence, Tuple
 
 import numpy as np
 from numpy.typing import NDArray
-from scipy.stats import pearsonr
 
 from ..dataclasses.interval import Bin
 from ._helpers import offset_via_slicing
@@ -10,7 +9,7 @@ from ._helpers import offset_via_slicing
 
 
 def pearson_correlation_offset_data(static_data: NDArray[np.float64], to_offset_data: NDArray[np.float64], offset_vectors: Iterable[NDArray[np.int64]]) -> NDArray[np.float64]:
-    return np.array([pearsonr(*map(np.ravel, offset_via_slicing(static_data, to_offset_data, v))).statistic for v in offset_vectors])
+    return np.array([np.corrcoef(*map(np.ravel, offset_via_slicing(static_data, to_offset_data, v)))[0,1] for v in offset_vectors])
 
 
 def radial_average(origin: Tuple[float,float], x_pos: NDArray[np.float64], y_pos: NDArray[np.float64], data: NDArray[np.float64], radial_bins: Sequence[Bin]) -> NDArray[np.float64]:
