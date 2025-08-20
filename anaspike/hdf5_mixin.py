fd3be0b09b43535c20a155d8bc68582193537be7
Anton Zickler <azic02@proton.me>
2025-08-20 16:11:01 +0200

    First implementation of HDF5Mixin

---
+++ anaspike/hdf5_mixin.py
@@ -0,0 +1,60 @@
+from typing import Type, TypeVar, Union, cast, Dict, List
+import inspect
+
+import h5py
+import numpy as np
+from numpy.typing import NDArray
+
+
+
+SupportedBaseTypes = Union[int, NDArray[np.float64]]
+
+def _load_supported_base_type(hdf5_obj: h5py.Dataset) -> SupportedBaseTypes:
+    if '__class__' not in hdf5_obj.attrs:
+        raise ValueError("HDF5 object does not contain '__class__' attribute.")
+    
+    class_name = hdf5_obj.attrs['__class__'] #type: ignore
+   
+    if class_name == 'int':
+        return int(hdf5_obj[()]) #type: ignore
+    elif class_name == 'numpy.ndarray':
+        return np.array(hdf5_obj[:], dtype=np.float64) #type: ignore
+    else:
+        raise ValueError(f"Unsupported class type '{class_name}' found in HDF5 object.")
+
+def _save_supported_base_type(hdf5_obj: h5py.Group, name: str, value: SupportedBaseTypes) -> None:
+    if isinstance(value, int):
+        ds = hdf5_obj.create_dataset(name, data=value) #type: ignore
+        ds.attrs.create('__class__', 'int') #type: ignore
+    elif isinstance(value, np.ndarray):
+        ds = hdf5_obj.create_dataset(name, data=value) #type: ignore
+        ds.attrs.create('__class__', 'numpy.ndarray') #type: ignore
+    else:
+        raise ValueError(f"Unsupported variable '{name}' of type '{type(value)}' for saving to HDF5.")
+
+T = TypeVar('T', bound='HDF5Mixin')
+
+class HDF5Mixin:
+    init_args: List[str] = []
+
+    def __init_subclass__(cls):
+        parameters = list(inspect.signature(cls.__init__).parameters.keys())
+        cls.init_args = [p for p in parameters if not p.startswith('*') and p != 'self']
+
+    @classmethod
+    def from_hdf5(cls: Type[T], hdf5_obj: Union[h5py.File, h5py.Group, h5py.Dataset, h5py.Datatype]) -> T:
+        if not isinstance(hdf5_obj, (h5py.Group, h5py.File)):
+            raise TypeError("Expected hdf5_obj to be a Group or File.")
+        kwargs: Dict[str, SupportedBaseTypes] = {}
+        for k in cls.init_args:
+            if k in hdf5_obj:
+                v = hdf5_obj[k]
+                kwargs[k] = _load_supported_base_type(cast(h5py.Dataset, v))
+        return cls(**kwargs)
+
+    def to_hdf5(self, hdf5_obj: Union[h5py.File, h5py.Group], name: str) -> None:
+        group = hdf5_obj.create_group(name) # type: ignore
+        for k in self.init_args:
+            v = getattr(self, k)
+            _save_supported_base_type(group, k, v)
+
---
+++ tests/test_hdf5_mixin.py
@@ -0,0 +1,41 @@
+import unittest
+
+import h5py
+
+from anaspike.hdf5_mixin import HDF5Mixin
+
+
+
+class TestHDF5Mixin(unittest.TestCase):
+    def test_int_member(self):
+        class DummyClass(HDF5Mixin):
+            def __init__(self, value: int):
+                self.value = value
+                
+
+        instance = DummyClass(value=42)
+        with h5py.File('test.h5', 'w') as f:
+            instance.to_hdf5(f, 'dummy')
+
+        with h5py.File('test.h5', 'r') as f:
+            loaded_instance = DummyClass.from_hdf5(f['dummy'])
+
+        self.assertEqual(instance.value, loaded_instance.value)
+
+    def test_ndarray_member(self):
+        import numpy as np
+        from numpy.typing import NDArray
+        class DummyClass(HDF5Mixin):
+            def __init__(self, array: NDArray[np.float64]):
+                self.array = array
+
+        array_data = np.array([1.0, 2.0, 3.0])
+        instance = DummyClass(array=array_data)
+
+        with h5py.File('test.h5', 'w') as f:
+            instance.to_hdf5(f, 'dummy')
+
+        with h5py.File('test.h5', 'r') as f:
+            loaded_instance = DummyClass.from_hdf5(f['dummy'])
+
+        np.testing.assert_array_equal(instance.array, loaded_instance.array)
