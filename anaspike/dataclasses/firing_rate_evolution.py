b492c68dd1e7282f0b723d34037caea966943fcc
Anton Zickler <azic02@proton.me>
2025-08-20 23:11:38 +0200

    Implement FiringRateEvolution class

---
+++ anaspike/dataclasses/firing_rate_evolution.py
@@ -0,0 +1,32 @@
+import numpy as np
+from numpy.typing import NDArray
+
+from ..hdf5_mixin import HDF5Mixin
+from ..dataclasses.histogram import ContigBins
+
+
+
+class FiringRateEvolution(HDF5Mixin):
+    def __init__(self, time_bins: ContigBins, firing_rates: NDArray[np.float64]):
+        if firing_rates.ndim != 2:
+            raise ValueError("`firing_rates` must be a 2D array.")
+        if len(time_bins) != firing_rates.shape[0]:
+            raise ValueError("First dimension of `firing_rates` must match length of `time_bins`.")
+
+        self.__time_bins = time_bins
+        self.__firing_rates = firing_rates
+
+    @property
+    def time_bins(self) -> ContigBins:
+        return self.__time_bins
+
+    @property
+    def firing_rates(self) -> NDArray[np.float64]:
+        return self.__firing_rates
+
+    def along_time_dim(self):
+        return self.__firing_rates
+
+    def along_neuron_dim(self):
+        return self.__firing_rates.T
+
---
+++ tests/test_firing_rate_evolution.py
@@ -0,0 +1,43 @@
+import unittest
+
+import numpy as np
+
+from anaspike.dataclasses.interval import Interval
+from anaspike.dataclasses.histogram import EquiBins
+from anaspike.dataclasses.firing_rate_evolution import FiringRateEvolution
+
+
+
+class TestFiringRateEvolution(unittest.TestCase):
+    def setUp(self):
+        self.firing_rates = np.array([[1.0, 2.0, 3.0],
+                                      [4.0, 5.0, 6.0],
+                                      [7.0, 8.0, 9.0]])
+        self.firing_rate_evolution = FiringRateEvolution(
+            time_bins=EquiBins.from_interval_with_median_values(Interval(start=0.0, end=3.0), n=3),
+            firing_rates=np.array(self.firing_rates),
+        )
+
+    def test_hdf5(self):
+        import h5py
+        with h5py.File('test_firing_rate_evolution.h5', 'w') as f:
+            self.firing_rate_evolution.to_hdf5(f, 'firing_rate_evolution')
+        with h5py.File('test_firing_rate_evolution.h5', 'r') as f:
+            loaded = FiringRateEvolution.from_hdf5(f['firing_rate_evolution'])
+        np.testing.assert_array_equal(loaded.firing_rates, self.firing_rate_evolution.firing_rates)
+        np.testing.assert_array_equal(loaded.time_bins.bin_edges, self.firing_rate_evolution.time_bins.bin_edges)
+
+    def test_along_time_dim(self):
+        for actual, expected in zip(
+            self.firing_rate_evolution.along_time_dim(),
+            self.firing_rates
+        ):
+            np.testing.assert_array_equal(actual, expected)
+
+    def test_along_neuron_dim(self):
+        for actual, expected in zip(
+            self.firing_rate_evolution.along_neuron_dim(),
+            self.firing_rates.T
+        ):
+            np.testing.assert_array_equal(actual, expected)
+
