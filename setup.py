b51efd07ef28b9c9be03248fbd074288578ec275
Anton Zickler <azic02@proton.me>
2025-09-04 16:25:25 +0200

    Increase version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='anaspike',
-    version='0.5.0',
+    version='0.6.0',
     packages=find_packages(),
     package_data={
         'anaspike': ['anaspike/py.py.typed']
