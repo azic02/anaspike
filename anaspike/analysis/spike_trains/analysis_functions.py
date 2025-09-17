8817f9ee86f368b890063fa0cf9dbcb94bc3abb1
Anton Zickler <azic02@proton.me>
2025-09-17 16:17:57 +0200

    Remove duplicate function
    ...already implemented more fittingly in the spike_counts module.

--- anaspike/analysis/spike_trains/analysis_functions.py
+++ anaspike/analysis/spike_trains/analysis_functions.py
@@ -5,9 +5,7 @@ import numpy as np
 from ...dataclasses.spike_train import SpikeTrainArray
 from ...dataclasses.histogram import Histogram
 from ...dataclasses.bins import ContigBins1D
-from ...dataclasses.interval import Interval
 from ...dataclasses.grid import Grid1D
-from ...functions.spike_derived_quantities import spike_counts
 
 
 
@@ -24,13 +22,4 @@ def construct_interspike_interval_histogram(spike_trains: SpikeTrainArray,
     per_neuron_histograms = (Histogram.construct_by_counting(t_bins, isi) for isi in inter_spike_intervals)
     all_neurons_histogram = Histogram(t_bins, np.sum([h.counts for h in per_neuron_histograms], axis=0))
     return all_neurons_histogram
-
-def calculate_active_neuron_fraction(spike_trains: SpikeTrainArray, t_interval: Interval, thresh: int=1) -> float:
-    counts = np.squeeze(spike_counts(spike_trains, t_interval))
-    if counts.shape != (spike_trains.n_neurons, ):
-        raise ValueError(f"Shape mismatch: counts.shape: {counts.shape}, (spike_trains.n_neurons, ): ({spike_trains.n_neurons}, )")
-    active_mask = counts >= thresh
-    n_active = np.sum(active_mask)
-    n_total = spike_trains.n_neurons
-    return float(n_active) / float(n_total)
 
--- tests/test_spike_trains_analysis_functions.py
+++ tests/test_spike_trains_analysis_functions.py
@@ -2,7 +2,6 @@ import unittest
 
 import numpy as np
 
-from anaspike.dataclasses.interval import Interval
 from anaspike.dataclasses.bins import ContigBins1D
 from anaspike.dataclasses.spike_train import SpikeTrainArray
 from anaspike.dataclasses.grid import Grid1D
@@ -47,53 +46,6 @@ class TestConstructInterspikeIntervalHistogram(unittest.TestCase):
         np.testing.assert_array_equal(result.counts, self.expected_counts)
         np.testing.assert_array_almost_equal(result.edges, self.expected_bin_edges)
         np.testing.assert_array_almost_equal(result.labels, self.expected_bin_labels)
-
-
-class CalculateActiveNeuronFractionTestCase(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.t_interval = Interval(0, 2)
-        cls.thresh = 1
-
-class TestCalculateActiveNeuronFractionSomeActive(CalculateActiveNeuronFractionTestCase):
-    def setUp(self):
-        self.spike_trains = SpikeTrainArray([np.array([0, 0.1]),
-                                                  np.array([1.0, 1.2]),
-                                                  np.array([0.3]),
-                                                  np.array([])])
-
-        self.expected_fraction = 3. / 4
-
-    def test(self):
-        from anaspike.analysis.spike_trains import calculate_active_neuron_fraction
-        result = calculate_active_neuron_fraction(self.spike_trains, self.t_interval, self.thresh)
-        self.assertAlmostEqual(result, self.expected_fraction)
-
-class TestCalculateActiveNeuronFractionNoneActive(CalculateActiveNeuronFractionTestCase):
-    def setUp(self):
-        self.spike_trains = SpikeTrainArray([np.array([]),
-                                             np.array([]),
-                                             np.array([])])
-
-        self.expected_fraction = 0.0
-
-    def test(self):
-        from anaspike.analysis.spike_trains import calculate_active_neuron_fraction
-        result = calculate_active_neuron_fraction(self.spike_trains, self.t_interval, self.thresh)
-        self.assertAlmostEqual(result, self.expected_fraction)
-
-class TestCalculateActiveNeuronFractionAllActive(CalculateActiveNeuronFractionTestCase):
-    def setUp(self):
-        self.spike_trains = SpikeTrainArray([np.array([0]),
-                                             np.array([1.0]),
-                                             np.array([0.3])])
-
-        self.expected_fraction = 1.0
-
-    def test(self):
-        from anaspike.analysis.spike_trains import calculate_active_neuron_fraction
-        result = calculate_active_neuron_fraction(self.spike_trains, self.t_interval, self.thresh)
-        self.assertAlmostEqual(result, self.expected_fraction)
 
 
 if __name__ == '__main__':
