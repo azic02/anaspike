3fed84a5614f247232bd77a3615133ae9af92190
Anton Zickler <azic02@proton.me>
2025-09-17 18:24:09 +0200

    Use spike_counts module

--- anaspike/analysis/time_averaged_firing_rate/time_averaged_firing_rate.py
+++ anaspike/analysis/time_averaged_firing_rate/time_averaged_firing_rate.py
@@ -4,6 +4,7 @@ import numpy as np
 from numpy.typing import NDArray, DTypeLike
 
 from ...hdf5_mixin import HDF5Mixin
+from ..spike_counts import SpikeCounts
 from ..spike_trains import SpikeTrains
 from ...dataclasses.interval import Interval
 from ...dataclasses.coords2d import Coords2D
@@ -18,9 +19,13 @@ class TimeAveragedFiringRate(Field2D[np.float64], HDF5Mixin):
         super().__init__(coords, firing_rates)
 
     @classmethod
+    def from_spike_counts(cls, spike_counts: SpikeCounts, time_window: Interval, time_unit: float=1.e-3):
+        return cls(spike_counts.coords, spike_counts.counts / (time_window.width * time_unit))
+
+    @classmethod
     def from_spike_trains(cls, spike_trains: SpikeTrains, time_window: Interval, time_unit: float=1.e-3):
-        spike_counts = np.array([np.sum(time_window.contains(st)) for st in spike_trains])
-        return cls(spike_trains.coords, spike_counts / (time_window.width * time_unit))
+        spike_counts = SpikeCounts.from_spike_trains(spike_trains, time_window)
+        return cls.from_spike_counts(spike_counts, time_window, time_unit)
 
     @property
     def firing_rates(self) -> NDArray[np.float64]:
