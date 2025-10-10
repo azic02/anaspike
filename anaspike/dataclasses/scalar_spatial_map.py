a7d08967a790f6624a5206f12fb74e2e353fc6b1
Anton Zickler <azic02@proton.me>
2025-10-11 01:40:23 +0200

    Implement ScalarSpatialMap.morans_i

--- anaspike/dataclasses/scalar_spatial_map.py
+++ anaspike/dataclasses/scalar_spatial_map.py
@@ -5,6 +5,8 @@ from numpy.typing import NDArray
 
 from .spatial_map import SpatialMap
 from .coords2d import Coords2D
+from ..functions.statistical_quantities import morans_i as morans_i_untyped
+from ..functions._helpers import calculate_pairwise_2d_euclidean_distances
 
 
 CoordsT = TypeVar("CoordsT", bound=Coords2D)
@@ -13,4 +15,11 @@ class ScalarSpatialMap(SpatialMap[CoordsT, np.float64]):
         if values.ndim != 1:
             raise ValueError("values must be a 1D array")
         super().__init__(coords, values)
+
+
+def morans_i(sm: ScalarSpatialMap[Coords2D]) -> float:
+    distances = calculate_pairwise_2d_euclidean_distances(sm.coords.x, sm.coords.y)
+    np.fill_diagonal(distances, 1.)
+    weights = np.power(distances, -1)
+    return morans_i_untyped(sm.values, weights)
 
